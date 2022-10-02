5dedfff4183a70a1c5925b6f7e3fd86a6bf3775e
ManuelYanezA <manuel.yanez1501@alumnos.ubiobio.cl>
2022-10-02 19:01:44 -0300

    Added player sprint

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float sprintAdd = 3f;
+    public float currentSpeed;
     public Rigidbody2D rb;
     Vector2 movement;
 
     void Update()
     {
         //Inputs
+        if(Input.GetKey(KeyCode.X))
+        {
+            currentSpeed = moveSpeed + sprintAdd;
+        }
+        else
+        {
+            currentSpeed = moveSpeed;
+        }
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
     }
 
     void FixedUpdate(){
         //Physics
-        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + movement.normalized * currentSpeed * Time.fixedDeltaTime);
     }
 }

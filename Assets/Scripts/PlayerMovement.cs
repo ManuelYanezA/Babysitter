b08df57013dbb3b65e53d3875546f0958e90cbb5
Bastillas255 <alvarorocuant@gmail.com>
2022-10-04 05:10:32 +0900

    Sprite Animations in

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,7 +8,13 @@ public class PlayerMovement : MonoBehaviour
     public float sprintAdd = 3f;
     private float currentSpeed;
     public Rigidbody2D rb;
+    public Animator anim;
     Vector2 movement;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     void Update()
     {
@@ -23,6 +29,18 @@ public class PlayerMovement : MonoBehaviour
         }
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        //feeding values to animator
+        if (movement.magnitude > 0.5)
+            anim.SetBool("IsMoving", true);
+        else
+            anim.SetBool("IsMoving", false);
+
+        //changing sprite directions
+        if (movement.x<=0)
+            transform.localScale = new Vector2(1f, 1f);
+        else
+            transform.localScale = new Vector2(-1f, 1f);
     }
 
     void FixedUpdate(){
